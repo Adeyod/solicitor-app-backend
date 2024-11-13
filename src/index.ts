147dc1749e1a5230d2a355fdfe7152873711a30a
Ayodeji Adebolu <ayodejiadebolu@gmail.com>
2024-11-13 09:54:56 +0100

    allowed origins

--- src/index.ts
+++ src/index.ts
@@ -32,7 +32,7 @@ const corsOptions: CorsOptions = {
     if (allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
-      callback(new Error('Not allowed by CORS'));
+      callback(new Error('Access Not allowed by CORS'));
     }
   },
   credentials: true,
