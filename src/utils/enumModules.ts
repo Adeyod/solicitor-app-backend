03e3d9ce9bb8729a2b37c4ad85b50b1e33d98c41
Ayodeji Adebolu <ayodejiadebolu@gmail.com>
2024-11-13 10:33:50 +0100

    added new admin email

--- src/utils/enumModules.ts
+++ src/utils/enumModules.ts
@@ -2,7 +2,11 @@ const memberRole = ['client', 'worker', 'admin', 'lawyer'] as const;
 const caseStatusEnum = ['Open', 'Closed', 'Pending', 'Completed'];
 const appointmentPurposeEnum = ['Scheduled', 'Completed', 'Canceled'];
 const caseTypeEnum = ['Civil', 'Criminal', 'Family', 'Corporate', 'Property'];
-const allowedEmails = ['ayodejiadebolu@gmail.com', 'votinggivers@gmail.com'];
+const allowedEmails = [
+  'ayodejiadebolu@gmail.com',
+  'votinggivers@gmail.com',
+  'taiwo.tope@proton.me',
+];
 
 type MemberRole = (typeof memberRole)[number];
 
